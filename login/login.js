c4dfa90a9c2ab7208744a74d17d2e3fa5405fc23
ngogiaphat <ngogiaphat0802@gmail.com>
2022-05-16 22:42:30 +0700

    update

--- login/login.js
+++ login/login.js
@@ -1,96 +1,85 @@
 // chuyển đổi trong login và signup
 let x = document.getElementById("login"),
 y = document.getElementById("sign-up");
-
 const toggleBtn = document.querySelector(".toggle-btn")
-
-function login() {
+function login() 
+{
     x.style.left = '0';
     y.style.left = '40rem';
     toggleBtn.querySelector(".login").classList.add("active");
     toggleBtn.querySelector(".sign-up").classList.remove("active");
 
 }
-
-function signUp() {
+function signUp() 
+{
     x.style.left = '-40rem';
     y.style.left = '0';
     toggleBtn.querySelector(".login").classList.remove("active");
     toggleBtn.querySelector(".sign-up").classList.add("active");
 }
-
-
 // Validator
-function Validator(options) {
+function Validator(options) 
+{
     let selectorRules = {};
-
-
     // hàm xử lí xem có input có hợp lệ hay không 
-    function validate(inputElement, rule) {
+    function validate(inputElement, rule) 
+    {
         let errorMessage;
         let errorElement = inputElement.parentElement.querySelector(".message");
-        
-
         let rules = selectorRules[rule.selector];
-        
         // lặp qua từng rule
-        for(let i = 0; i < rules.length; ++i) {
+        for(let i = 0; i < rules.length; ++i) 
+        {
             errorMessage =rules[i](inputElement.value);
             if(errorMessage) break;
         }
-
-        if(errorMessage) {
+        if(errorMessage) 
+        {
             errorElement.innerText = errorMessage;
             inputElement.parentElement.classList.add('error');
-        } else {
+        } 
+        else 
+        {
             errorElement.innerText = '';
             inputElement.parentElement.classList.remove('error');
         }
-
         return errorMessage;
     }
-
-    
-
-
-
-
-
     // hàm xử lí từng rule xem có hợp lệ hay không
     let formElement = document.querySelector(options.form);
-
-    if(formElement) {
-
+    if(formElement) 
+    {
         // nhấp nút submit
         let btnSubmit = formElement.querySelector(".submit-btn");
         btnSubmit.onclick = (e) => {
             let isValid = true;
-
-            options.rules.forEach(function (rule) {
+            options.rules.forEach(function (rule) 
+            {
                 let inputElement = formElement.querySelector(rule.selector);
                 console.log(validate(inputElement, rule));
-                if(validate(inputElement, rule)) {
+                if(validate(inputElement, rule)) 
+                {
                     isValid = false;
                 }
             })
-            
-            if(!isValid) {
+            if(!isValid) 
+            {
                 e.preventDefault();
             }
         }
-
-
         options.rules.forEach((rule) => {
             let inputElement = formElement.querySelector(rule.selector);
             let errorElement = inputElement.parentElement.querySelector(".message");
-
-            if (Array.isArray(selectorRules[rule.selector])) {
+            if (Array.isArray(selectorRules[rule.selector])) 
+            {
                 selectorRules[rule.selector].push(rule.test)
-            } else {
+            } 
+            else 
+            {
                 selectorRules[rule.selector] = [rule.test];
             }
-
-            if(inputElement) {
+            if(inputElement) 
+            {
                 inputElement.onblur = () => {
                     validate(inputElement, rule);
                 }
@@ -102,48 +91,44 @@ function Validator(options) {
         });
     }
 }
-
 // kiểm tra có bỏ trống hay không
 Validator.isRequired = (selector) => {
     return {
         selector: selector,
-        test: function(value) {
+        test: function(value) 
+        {
             return (value) ? undefined : 'Không được bỏ trống';
         },
     }
 }
-
 // kiểm tra có phải email
 Validator.isEmail = (selector) => {
     return {
         selector: selector,
-        test: function(value) {
+        test: function(value) 
+        {
             let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
             return res.test(value) ? undefined : 'Đây không phải là email';
         },
     }
 }
-
 //Kiểm tra có dủ độ dài hay không
 Validator.minLength = (selector, min) => {
     return {
         selector: selector,
-        test: function(value) {
+        test: function(value) 
+        {
             return (value.length >= min) ? undefined : `Không đủ ${min} kí tự`; 
         },
     }
 }
-
 // Kiểm tra giá trị 
 Validator.isConfirmed = (selector, getConfirmValue, message) => {
     return {
         selector: selector,
-        test: function(value) {
+        test: function(value) 
+        {
             return (value ===  getConfirmValue() ) ? undefined : `${message} không đúng`;
         }
     }
 }
-
-
-
-
--- shop/product.js
+++ shop/product.js
@@ -1,44 +1,44 @@
 import products from '../all/data.json' assert {type: 'json'};
-
-
 // thêm sản phẩm vào giỏ hàng
 let items;
 if(localStorage.getItem('items')) {
     items = JSON.parse(localStorage.getItem('items'));
-} else {
+} 
+else 
+{
     items = [];
 }
-
 window.addItem = (e) => {
     let available = false;
     countItem();
     const quantity = e.parentElement.parentElement.querySelector("input").value;
-
-    if(items.length) {
+    if(items.length) 
+    {
         items.forEach((item) => {
-            if(item.id == e.id) {
+            if(item.id == e.id) 
+            {
                 item.quantity = parseInt(item.quantity) + parseInt(quantity);
                 localStorage.setItem('items', JSON.stringify(items));
                 available = true;
             };
         });
-        if(!available) {
+        if(!available) 
+        {
             products[index].quantity = quantity;
             items.push(products[index])
             localStorage.setItem('items', JSON.stringify(items))
         }
-    } else {
+    } 
+    else 
+    {
         products[index].quantity = quantity;
         items.push(products[index])
         localStorage.setItem('items', JSON.stringify(items))
     }
     countItem();
 }
-
-
 const index = localStorage.getItem('item')
 let article = document.querySelector("article");
-
 article.innerHTML = `
 <div class="product-wrapper">
 <div class="image">
@@ -55,7 +55,6 @@ article.innerHTML = `
             <i class="fas fa-star"></i>
         </div>
         <div class="price">${products[index].price}</div>
-
         <div class="option">
             <select name="" id="size">
                 <option value="S">S</option>
